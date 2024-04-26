cab1784b85091cdec885c57a3910b7066ad9da7a
Uryel Jó de Lucca Araujo de Oliveira <uryeljodelucca11@gmail.com>
2024-04-26 13:42:30 -0300

    Refatoração

--- TrabalhoCelio/Game.cpp
+++ TrabalhoCelio/Game.cpp
@@ -18,12 +18,13 @@
 #include <iostream>
 
 ALLEGRO_DISPLAY *telaGame = NULL;
-ALLEGRO_TIMER *fps = NULL;
+ALLEGRO_TIMER *fps = NULL, *fpsImagem = NULL;
 ALLEGRO_EVENT_QUEUE* filaEventos = NULL;
 
-Mapa mapa = Mapa();
-Player player;
+int delayAnimacao = 0;
 
+Mapa mapa = Mapa();
+Player player = Player(0, 0, 115, 72, 0, mapa.alturaTela - mapa.alturaTela, Moveset(), 0);
 
 void inicializacao();
 void encerramento();
@@ -34,13 +35,14 @@ void gravidade();
 int main() {
 	
 	inicializacao();
-	player.carregarImagemPersonagem(al_load_bitmap("ParadoF.png"));
+	player.carregarImagemPersonagem(al_load_bitmap("Parado.png"));
 	
 	limparTela();
 	atualizarTela();
 	
 	//Iniciar o Timer
 	al_start_timer(fps);
+	al_start_timer(fpsImagem);
 
 	while (true)
 	{
@@ -50,15 +52,18 @@ int main() {
 		if (evento.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
 			break;
 		}
+
 		if (evento.type == ALLEGRO_EVENT_KEY_DOWN) {
 			switch(evento.keyboard.keycode) {
 				case ALLEGRO_KEY_LEFT:
 					player.movesetPlayer.movendoEsquerda = true;
 					player.movesetPlayer.movendoDireita = false;
+					player.flags = ALLEGRO_FLIP_HORIZONTAL;
 					break;
 				case ALLEGRO_KEY_RIGHT:
 					player.movesetPlayer.movendoDireita = true;
 					player.movesetPlayer.movendoEsquerda = false;
+					player.flags = 0;
 					break;
 			}
 		}
@@ -70,23 +75,41 @@ int main() {
 			player.puloPersonagem();
 		}
 		
-		player.movimentacaoDireita();
-		player.movimentacaoEsquerda();
+			player.movimentacaoDireita();
+			player.movimentacaoEsquerda();
+		
+		
 
 		if (evento.type == ALLEGRO_EVENT_KEY_UP) {
 			switch (evento.keyboard.keycode) {
 				case ALLEGRO_KEY_LEFT:
 					player.movesetPlayer.movendoEsquerda = false;
-					player.imagemPersonagem = al_load_bitmap("ParadoT.png");
 					break;
 				case ALLEGRO_KEY_RIGHT:
 					player.movesetPlayer.movendoDireita = false;
-					player.imagemPersonagem = al_load_bitmap("ParadoF.png");
 					break;
 			}
+			player.imagemPersonagem = al_load_bitmap("Parado.png");
 		}
+
+		
+
+		if (!player.movesetPlayer.movendoDireita && !player.movesetPlayer.movendoEsquerda && !player.movesetPlayer.pulando) {
+			delayAnimacao++;
+			if (delayAnimacao == 30) {
+				if (player.frame_x < 115) {
+					player.frame_x += 115;
+				}
+				else {
+					player.frame_x = 0;
+				}
+				delayAnimacao = 0;
+			}
+			
+		}
+		
 		limparTela();
-		al_draw_bitmap_region(player.imagemPersonagem, player.frame_x, player.frame_y, player.alturaPlayer, player.larguraPlayer, player.posicao_x_tela, player.posicao_y_tela, 0);
+		al_draw_bitmap_region(player.imagemPersonagem, player.frame_x, player.frame_y, player.alturaPlayer, player.larguraPlayer, player.posicao_x_tela, player.posicao_y_tela, player.flags);
 		atualizarTela();
 	}
 
@@ -94,7 +117,6 @@ int main() {
 }
 
 void inicializacao() {
-	player = Player(0, 0, 96, 72, 0, mapa.alturaTela - mapa.alturaTela, Moveset());
 	
 	//Inicialização do Allegro e dos Addons
 	al_init();
@@ -117,10 +139,17 @@ void inicializacao() {
 		return;
 	}
 
-	//Inicialização do Timer
+	//Inicialização do Timer do Game
 	fps = al_create_timer(1.0 / mapa.fpsGame);
 	if (!fps) {
 		al_show_native_message_box(telaGame, "ERRO", "ERRO", "Erro ao criar o timer", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return;
+	}
+
+	//Inicialização do Timer do Sprite
+	fpsImagem = al_create_timer(1.0 / player.fpsImagem);
+	if (!fpsImagem) {
+		al_show_native_message_box(telaGame, "ERRO", "ERRO", "Erro ao criar o timer da imagem", NULL, ALLEGRO_MESSAGEBOX_ERROR);
 		return;
 	}
 
@@ -134,6 +163,7 @@ void inicializacao() {
 	//Registro de Eventos
 	al_register_event_source(filaEventos, al_get_display_event_source(telaGame));
 	al_register_event_source(filaEventos, al_get_timer_event_source(fps));
+	al_register_event_source(filaEventos, al_get_timer_event_source(fpsImagem));
 	al_register_event_source(filaEventos, al_get_keyboard_event_source());
 }
 
@@ -142,6 +172,7 @@ void encerramento() {
 	al_destroy_display(telaGame);
 	al_destroy_bitmap(player.imagemPersonagem);
 	al_destroy_timer(fps);
+	al_destroy_timer(fpsImagem);
 	al_destroy_event_queue(filaEventos);
 }
 
@@ -156,7 +187,7 @@ void limparTela() {
 void gravidade() {
 	if (player.posicao_y_tela < mapa.alturaTela - 90) {
 		player.movesetPlayer.voando = true;
-		player.posicao_y_tela += 3;
+		player.posicao_y_tela += 2;
 	}
 	else {
 		player.movesetPlayer.voando = false;
--- TrabalhoCelio/Player.h
+++ TrabalhoCelio/Player.h
@@ -9,6 +9,8 @@ class Player
 		
 		ALLEGRO_BITMAP* imagemPersonagem = NULL;
 
+		const float fpsImagem = 2.f;
+
 		int frame_x = 0;
 		int frame_y = 0;
 
@@ -20,11 +22,13 @@ class Player
 		float posicao_x_tela = 0.f;
 		float posicao_y_tela = 0.f;
 
+		int flags = 0;
+
 		Moveset movesetPlayer;
 
 		Player() {};
 
-		Player(int fx, int fy, int lp, int ap, float px, float py, Moveset ms) {
+		Player(int fx, int fy, int lp, int ap, float px, float py, Moveset ms, int flags) {
 			this->frame_x = fx;
 			this->frame_y = fy;
 			this->larguraPlayer = lp;
@@ -32,6 +36,7 @@ class Player
 			this->posicao_x_tela = px;
 			this->posicao_y_tela = py;
 			this->movesetPlayer = ms;
+			this->flags = flags;
 		}
 
 		void carregarImagemPersonagem(ALLEGRO_BITMAP* imagemPersonagem);
@@ -48,12 +53,12 @@ void Player::carregarImagemPersonagem(ALLEGRO_BITMAP* imagemPersonagem) {
 void Player::movimentacaoEsquerda() {
 	
 	if (this->movesetPlayer.movendoEsquerda) {
-		this->imagemPersonagem = al_load_bitmap("AndandoT.png");
+		this->imagemPersonagem = al_load_bitmap("Andando.png");
 		this->posicao_x_tela -= 3;
 		this->delayTrocaDeFrame++;
 		if (this->delayTrocaDeFrame == 17) {
-			this->frame_x += 96;
-			if (this->frame_x == 384) {
+			this->frame_x += 115;
+			if (this->frame_x == 230) {
 				this->frame_x = 0;
 			}
 			this->delayTrocaDeFrame = 0;
@@ -63,12 +68,12 @@ void Player::movimentacaoEsquerda() {
 
 void Player::movimentacaoDireita() {
 	if(this->movesetPlayer.movendoDireita) {
-		this->imagemPersonagem = al_load_bitmap("AndandoF.png");
+		this->imagemPersonagem = al_load_bitmap("Andando.png");
 		this->posicao_x_tela += 3;
 		this->delayTrocaDeFrame++;
 		if (this->delayTrocaDeFrame == 17) {
-			this->frame_x += 96;
-			if (this->frame_x == 384) {
+			this->frame_x += 115;
+			if (this->frame_x == 230) {
 				this->frame_x = 0;
 			}
 			this->delayTrocaDeFrame = 0;
@@ -81,9 +86,16 @@ void Player::puloPersonagem() {
 		for (int i = 0; i < 40; i++) {
 			this->posicao_y_tela -= 2;
 			this->delayTrocaDeFrame++;
-			if (this->delayTrocaDeFrame == 2) {
+			if (this->delayTrocaDeFrame == 1) {
 				al_clear_to_color(al_map_rgb(255, 255, 255));
-				al_draw_bitmap_region(al_load_bitmap("ParadoF.png"), this->frame_x, this->frame_y, this->alturaPlayer, this->larguraPlayer, this->posicao_x_tela, this->posicao_y_tela, 0);
+				al_draw_bitmap_region(al_load_bitmap("Parado.png"), 
+					this->frame_x, 
+					this->frame_y, 
+					this->alturaPlayer, 
+					this->larguraPlayer, 
+					this->posicao_x_tela, 
+					this->posicao_y_tela, 
+					this->flags);
 				al_flip_display();
 				this->delayTrocaDeFrame = 0;
 			}
